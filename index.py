6a765f5d42c12ed86353b51d96c0c3234553e3e1
あめみん <63637891+Ameminn-python@users.noreply.github.com>
2020-09-09 10:57:38 +0900

    Update index.py

--- index.py
+++ index.py
@@ -1,1 +1,14 @@
 import discord
+import sys
+import io
+from discord.ext import commands
+import cgi
+token = "
+bot = commands.Bot(prefix="/")
+
+@bot.event
+async def on_ready():
+    print("ready")
+    
+
+
