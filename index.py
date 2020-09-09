ff93a415cc47c10e8816336506336cf00f134b4e
あめみん <63637891+Ameminn-python@users.noreply.github.com>
2020-09-09 13:18:04 +0900

    Update index.py

--- index.py
+++ index.py
@@ -1,14 +1,27 @@
-import discord
 import sys
 import io
-from discord.ext import commands
 import cgi
-token = "
-bot = commands.Bot(prefix="/")
+import discordbot
+form = cgi.FieldStorage()
 
-@bot.event
-async def on_ready():
-    print("ready")
-    
+sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
 
+
+user_id = form.getfirst('user_id')
+text_content = form.getfirst('text_cnt')
+result =  to_send(user_id,text_content)
+if result == 'error':
+    print('Content-type: text/html; charset=UTF-8')
+    print('')
+    print('送信に失敗しました')
+else:
+    print('Content-type: text/html; charset=UTF-8')
+    print('') 
+    print(user_id)
+    print(text_content)
+    print('送信に成功しました')
+
+
+
+                             
 
