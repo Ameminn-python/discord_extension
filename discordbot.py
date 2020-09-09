c9ef9f98ad9e253e819af57605f706ec0f2246c7
あめみん <63637891+Ameminn-python@users.noreply.github.com>
2020-09-09 13:30:00 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,1 +1,14 @@
+import discord 
+from discord.ext import commands
 
+bot = commands.Bot(prefix='/')
+
+async def to_send(id:int,ctn:str):
+    try:
+        who = bot.get_user(id)
+        await who.send(ctn)
+    except:
+        return 'error'
+    else:
+        return 'succsess'
+    
